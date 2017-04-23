02cd0de1267acfd7cf777ecf710a4a3fae74f21f
tiranid99 <ermakovichdmitriy@gmail.com>
2017-04-23 15:08:57 +0300

    new features, but dont work

--- src/Unit1.pas
+++ src/Unit1.pas
@@ -32,21 +32,24 @@ type
 var
   Form1: TForm1;
   // массив случайных простых чисел дл€ p и q
-  B: array [1..50] of integer;
+  B: array[1..50] of integer;
   // хеш строки
-  hashCode : Int64;
-  messager: String;
+  hashCode: Int64;
+  messager: string;
   // открыта€ экспонента
   eexp: integer;
   // секретна€ экспонента
   dexp: integer;
   // Ёлектронна€ подпись (Digital signature)
   s: int64;
-  p, q : integer;
-  n, eiler : Int64;
+  p, q: integer;
+  n, eiler: Int64;
   flag: boolean;
   res: integer;
-
+  MArray: array of integer;
+  SignArray: array of int64;
+  maxInt64: Int64;
+  maxM: integer;   // 2^21 -1
 
   //testers:
   tried: cardinal;
@@ -54,117 +57,121 @@ var
 
 implementation
 
-uses Math;
+uses
+  Math;
 
 {$R *.dfm}
 
 // решето Ёратосфена как процедура поиска больших простых чисел
 procedure reshetoErat();
-var  A: array [1..232] of boolean;
-      n, x, y: integer;
+var
+  A: array[1..232] of boolean;
+  n, x, y: integer;
 begin
-  A[1]:=false;
-  n:= 232;
-  Form1.Label2.Caption:= ' ';
-  for x:=2 to n do
-    a[x]:=true;
-  for x:=2 to n div 2 do
-    for y:= 2 to n div x do
-      a[x*y] := false;
-  y:=1;
-  for x:= n downto n - 200 do
-    if a[x] then
-      begin
-        B[y]:= x;
-        inc(y);
-      end;
+  A[1] := false;
+  n := 232;
+  Form1.Label2.Caption := ' ';
+  for x := 2 to n do
+    A[x] := true;
+  for x := 2 to n div 2 do
+    for y := 2 to n div x do
+      A[x * y] := false;
+  y := 1;
+  for x := n downto n - 200 do
+    if A[x] then
+    begin
+      B[y] := x;
+      inc(y);
+    end;
 end;
 
 
 // наш 16 битный хеш
-function hashH37(s: string):integer;
-var hash: integer;
-    i: cardinal;
+function hashH37(s: string): integer;
+var
+  hash: integer;
+  i: cardinal;
 begin
-  hash:= 139062143;
-  for i:= 1 to length(s) do
-    begin
-      hash:= 37 * hash + ord(s[i]);
-    end;
-  hash:= hash shr 16;
-  hashH37:= hash;
+  hash := 139062143;
+  for i := 1 to length(s) do
+  begin
+    hash := 37 * hash + ord(s[i]);
+  end;
+  hash := hash shr 16;
+  hashH37 := hash;
 
 end;
 
-function crc16(s:string):integer;
-var i, crc: integer;
+function crc16(s: string): integer;
+var
+  i, crc: integer;
 begin
-  crc:= 0;
-  for i:= 1 to length(s) do
+  crc := 0;
+  for i := 1 to length(s) do
   begin
-    crc:= crc + ord(s[i])*44111;
+    crc := crc + ord(s[i]) * 44111;
     //crc:= crc xor (crc shr 8);
 
   end;
 
-
-  Result:= crc;
+  Result := crc;
 end;
 
-
-
-
-function eightbitxorsum(mess:string):integer;
-var i, sum, scode: integer;
+function eightbitxorsum(mess: string): integer;
+var
+  i, sum, scode: integer;
 begin
-  sum:= ord(mess[1]);
-  for i:= 2 to length(mess) do
-    sum:= sum xor ord(mess[i]);
+  sum := ord(mess[1]);
+  for i := 2 to length(mess) do
+    sum := sum xor ord(mess[i]);
 
-  Result:= sum;
+  Result := sum;
 end;
 
 function ToBin(x: integer): string;
-const t:array[0..1] of char = ('0','1');
-var res:string;
-    d:0..1;
+const
+  t: array[0..1] of char = ('0', '1');
+var
+  res: string;
+  d: 0..1;
 begin
-  res:='';
-  while (x<>0) do
-    begin
-      d:=x mod 2 ;
-      res:=t[d]+res;
-      x:=x div 2 ;
-    end;
-  Result:=res;
+  res := '';
+  while (x <> 0) do
+  begin
+    d := x mod 2;
+    res := t[d] + res;
+    x := x div 2;
+  end;
+  Result := res;
 end;
 
 // хеширование строки методом SDBM (х32) с хорошим распределением по всем битам (мало коллизий)
-function SDBM(mess:String):int64;
-var i:cardinal;
-    hash:int64;
+function SDBM(mess: string): int64;
+var
+  i: cardinal;
+  hash: int64;
 begin
-  hash:=0;
-  for i:=1 to Length(mess) do
-      hash:= ord(mess[i]) + (hash shl 6) + (hash shl 16) - hash;
-  Result:=hash;
+  hash := 0;
+  for i := 1 to Length(mess) do
+    hash := ord(mess[i]) + (hash shl 6) + (hash shl 16) - hash;
+  Result := hash;
 end;
 
-
-Procedure gcd_ext(a, b : integer; var x, y, gcd : integer);
-var x1,y1 : integer;
-Begin
-  If b=0 Then
-    Begin
-      gcd:=a;
-      x:=1;
-      y:=0;
-      Exit
-    End;
+procedure gcd_ext(a, b: integer; var x, y, gcd: integer);
+var
+  x1, y1: integer;
+begin
+  if b = 0 then
+  begin
+    gcd := a;
+    x := 1;
+    y := 0;
+    Exit
+  end;
   gcd_ext(b, a mod b, x1, y1, gcd);
   x := y1;
   y := x1 - (a div b) * y1
-End;
+end;
 
 function Znak(Val: Integer): Char;
 begin
@@ -176,38 +183,36 @@ end;
 
 
 // функци€ возведени€ в степень (дл€ положительных чисел и степеней.
-function pow(base, up:int64): int64;
-var i: cardinal;
+function pow(base, up: int64): int64;
+var
+  i: cardinal;
 begin
   if (base = 1) or (up = 0) then
-    Result:= 1
+    Result := 1
   else
+  begin
+    Result := base;
+    for i := 2 to up do
     begin
-      Result:= base;
-      for i:= 2 to up do
-        begin
-          Result:= base * Result;
-          if (result < 0) then
-            begin
-              flag:= true;
-              break;
-            end;
-        end;
+      Result := base * Result;
+      if (result < 0) then
+      begin
+        flag := true;
+        break;
+      end;
     end;
+  end;
 end;
 
-
-
-function toInt(ch: char):integer;
+function toInt(ch: char): integer;
 begin
-if ch = '0' then
-  Result:= 0
-else if ch = '1' then
-  Result:= 1
-else
-  Result:= 9999;
+  if ch = '0' then
+    Result := 0
+  else if ch = '1' then
+    Result := 1
+  else
+    Result := 9999;
 end;
-
 
 procedure generateRsaSignature();
 begin
@@ -216,64 +221,73 @@ end;
 
 // uses Pow()
 procedure genDigitalSignature();
-var strdexp: string;
-    i, k: integer;
-    m: int64;
+var
+  strdexp: string;
+  i, k: integer;
+  m: int64;
 begin
 //949666443
-  strdexp:= ToBin(dexp);
-  m:= hashCode;
-  k:= length(strdexp);
-  for i:= 2 to k do
+  strdexp := ToBin(dexp);
+  m := hashCode;
+  k := length(strdexp);
+  for i := 2 to k do
+  begin
+    m := (m * m * pow(hashCode, toInt(strdexp[k - i + 1])));
+    if (m < 1) then
     begin
-      m:= (m*m * pow(hashCode, toInt(strdexp[k-i+1]))) mod n;
+      m := -1;
+      Break;
     end;
-  s:= m;
+    m := m mod n;
+  end;
+  s := m;
 
 
-
+  Form1.Label1.Caption:= IntToStr(s);
+  Form1.Label1.Visible:= True;
   //s := Pow(hashCode, dexp) mod n;
 
 end;
 
-function mode(a,b : Real) : Real;
+function mode(a, b: Real): Real;
 begin
   if (a < b) then
-    Result:=a
+    Result := a
   else
   begin
-    Result:= Round(a) mod Round(b);
+    Result := Round(a) mod Round(b);
   end;
 end;
 
-function NOD(a,b : Real) : Real;
-var t : Real;
+function NOD(a, b: Real): Real;
+var
+  t: Real;
 begin
-  while(b > 0) do
+  while (b > 0) do
   begin
-    t := mode(a,b);
+    t := mode(a, b);
     a := b;
     b := t;
   end;
-  Result:=a;
+  Result := a;
 end;
 
-
-procedure generateKeys();
-var aa, bb, nod : integer;
+procedure generateKeys(minimalN: integer);
+var
+  aa, bb, nod: integer;
 begin
   Randomize();
 
-
   p := 0;
   q := 0;
-  while ((p = q) or (p = 0) or (q = 0)) do
+  while ((p = q) or (p = 0) or (q = 0) or (n < minimalN * 10) or (n > minimalN * 100)) do
   begin
-    p := RandomFrom(B);
-    q := RandomFrom(B);
+    p := RandomFrom(b);
+    q := RandomFrom(b);
+    n := p * q;
   end;
-  n := p*q;
-  eiler := (p-1)*(q-1);
+
+  eiler := (p - 1) * (q - 1);
 
   eexp := 1;
   nod := 0;
@@ -292,135 +306,111 @@ begin
  }
 
   dexp := (aa mod eiler + eiler) mod eiler;
-  
+
+ {
   Form1.Label6.Caption := '  before first ' +  IntToStr(aa) + '  before second   ' + IntToStr(bb);
   Form1.Label6.Visible:= True;
-
+  }
 
 end;
 
-function getProizvCheck(one, two : integer) : int64;
+function getProizvCheck(one, two: integer): int64;
 begin
-  Result:= pow(one, two);
+  Result := pow(one, two);
   if (Result < 0) then
-    Result:= -1;
-  Result:= Result mod n;
+    Result := -1;
+  Result := Result mod n;
 end;
 
-
-procedure createSign(mesage:string);
+procedure createSign(minimalN: integer);
 var
-  start, endd, full : integer;
+  start, endd, full: integer;
   hsh: int64;
-
 begin
+  {
   full:= 0;
   start:= GetTickCount();
+   }
 
-  {
-
-  hashCode:= hashH37(mesage);
+  //reshetoErat(minimalN*100);
   reshetoErat();
+  generateKeys(minimalN);
 
-  generateKeys();
-  s := getProizvCheck(hashCode, dexp);
-  //generateRsaSignature();
-
-  }
-  if not(Form1.Edit3.Text = '') then
-    hashCode := StrToInt(Form1.Edit3.Text)
-  else
-    hashCode := hashH37(mesage);
-  reshetoErat();
-  generateKeys();
-  {}
+{
   n := 33;
   eexp := 3;
   dexp := 7;
+}
 
-  {}
   //s := getProizvCheck(hashCode, dexp);
   genDigitalSignature();
 
   endd := GetTickCount();
-  full:= full + (endd - start);
+  full := full + (endd - start);
   //Form1.Label3.Caption:= Form1.Label3.Caption +   '  public and private keys: ' + IntToStr(endd - start);
-  Form1.Label3.Caption:= Form1.Label3.Caption + '; time: ' + IntToStr(full);
+  Form1.Label3.Caption := Form1.Label3.Caption + '; time: ' + IntToStr(full);
 end;
 
 procedure TForm1.Button1Click(Sender: TObject);
-var b,c,d,z, llength:integer;
-    a : Int64;
+var
+  b, c, d, z, index, llength: integer;
+  a: Int64;
+    // дл€ разбиени€ на много m
+  currentM: integer;
+    // счетчик дл€ разбиени€
+  k, i: Cardinal;
+  el, elements: integer;
+  mesage: string;
 begin
-messager:= 'абвгдежзклмнопрстуфх';
+  maxInt64 := 9223372036854775807;
+// 2^21 -1 ибо m^3 должно быть <= maxInt64
+  maxM := 2097151;
 
-messager:= messager + messager;
-messager:= messager + messager;
-{}
-messager:= messager + messager;
-{
-messager:= messager + messager;
+  if not (Form1.Edit3.Text = '') then
+  begin
+    Val(Form1.Edit3.Text, hashCode, index);
+    if (index <> 0) then
+    begin
+      hashCode := SDBM(Form1.Edit3.Text);
+    end;
+  end;
 
-messager:= messager + messager;
+  Form1.Label3.Caption := '';
 
-messager:= messager + messager;
+  Randomize();
+  currentM := hashCode;
+  if (hashCode > maxM) then
+  begin
+    k := 1;
+    elements := 0;
+    currentM := hashCode;
+    while (currentM > maxM) do
+    begin
+      Inc(k);
+      currentM := hashCode div k;
+    end;
+    SetLength(MArray, k);
+    if ((maxM - currentM) > 100) then
+    begin
+      for i := 0 to k - 2 do
+      begin
+        el := currentM + (Random(200) - 100);
+        elements := elements + el;
+        MArray[i] := el;
+      end;
+    // последний элемент
+      MArray[k - 1] := hashCode - elements;
+    end;
 
-messager:= messager + messager;
+  end
+// m < maxM
+  else
+  begin
+    SetLength(MArray, 1);
+    MArray[0] := hashCode;
+  end;
 
-messager:= messager + messager;
-{}{
-messager:= messager + messager;
-
-messager:= messager + messager;
-messager:= messager + messager;
-
-messager:= messager + messager;
-messager:= messager + messager;
-
-messager:= messager + messager;
-
-messager:= messager + messager;
-
-messager:= messager + messager;
-
-messager:= messager + messager;
-messager:= messager + messager;
-messager:= messager + messager;
-messager:= messager + messager;
-messager:= messager + messager;
-messager:= messager + messager;
-messager:= messager + messager;
-messager:= messager + messager;
-messager:= messager + messager;
-}
-
-llength:= Length(messager);
-Form1.Label3.Caption:= '';
-
-
-//NOD(20, (1/7));
-
-
-createSign(messager);
-
-
-// результаты вычислений не согласуютс€, подпись = 25, производна€ - 16
-// p, q = 3, 11
-// n = 33
-// ei = 20
-// e = 3 d = 7
-//a := pow(25, 3) mod 33;
-
-//Form1.Label3.Caption:= IntToStr(a);
-
-
-
-
-Label1.Caption:= '   hash = ' + IntToStr(hashCode) + ' при размере ' + IntTOStr(llength);
-Label2.Caption:= 's  = ' + IntTOStr(s);
-{}
-Label4.Caption:= 'triesToNotSimple + not zeros  = ' + IntToStr(tried)
-+ '  dexp = ' + IntToStr(dexp) + '  eexp = ' + IntToStr(eexp) + '  n = ' + IntToStr(n);
+  createSign(currentM);
 end;
 
 // http://www.cyberforum.ru/delphi-beginners/thread1116790.html
@@ -433,30 +423,33 @@ end;
 // https://planetcalc.ru/3311/
 
 procedure TForm1.Button2Click(Sender: TObject);
-Var m,n,x,y,g, dex : integer;
+var
+  m, n, x, y, g, dex: integer;
 begin
   m := StrToInt(Edit1.Text);
   n := StrToInt(Edit2.Text);
   if (m < 0) or (n < 0) then
-    begin
-      ShowMessage('„исла должны быть натуральными');
-      Exit;
-    end;
+  begin
+    ShowMessage('„исла должны быть натуральными');
+    Exit;
+  end;
 
   gcd_ext(m, n, x, y, g);
 
   dex := (x mod n + n) mod n;
-  Form1.Label6.Caption := '  before first ' +  IntToStr(x) + '  before second   ' + IntToStr(y) + ' dex = ' + IntToStr(dex);
-  Form1.Label6.Visible:= True;
+ // Form1.Label6.Caption := '  before first ' +  IntToStr(x) + '  before second   ' + IntToStr(y) + ' dex = ' + IntToStr(dex);
+ // Form1.Label6.Visible:= True;
   ShowMessage(Format('gcd(%d, %d) = %d', [m, n, g]) + #13#10 + Format('%d * %d %s %d * %d = %d', [x, m, Znak(y), Abs(y), n, g]));
 end;
 
 procedure TForm1.Button3Click(Sender: TObject);
-var proizv : integer;
+var
+  proizv: integer;
 begin
-proizv := getProizvCheck(s, eexp);
-Label6.Caption:= IntToStr(proizv);
-Label6.Visible:= True;
+  proizv := getProizvCheck(s, eexp);
+  Label6.Caption := IntToStr(proizv);
+  Label6.Visible := True;
 end;
 
 end.
+
