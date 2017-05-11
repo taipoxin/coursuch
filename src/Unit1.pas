87a8fa052fb4491ea492486f5952117e6532d6e3
tiranid99 <ermakovichdmitriy@gmail.com>
2017-05-11 20:10:01 +0300

    almost done. Just todo saving another file with RSA

--- src/Unit1.pas
+++ src/Unit1.pas
@@ -10,7 +10,16 @@ type
   TForm1 = class(TForm)
     Button1: TButton;
     Edit3: TEdit;
+    Edit1: TEdit;
+    Label1: TLabel;
+    Edit2: TEdit;
+    Label2: TLabel;
+    Button2: TButton;
+    OpenDialog1: TOpenDialog;
+    Button3: TButton;
     procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -21,29 +30,7 @@ var
   Form1: TForm1;
   // массив случайных простых чисел для p и q
   B: array[1..50] of integer;
-  // хеш строки
-  hashCode: Int64;
-  messager: string;
-  // открытая экспонента
-  //eexp: integer;
-  // секретная экспонента
-  //dexp: integer;
-  // Электронная подпись (Digital signature)
-  //s: int64;
-  p, q: integer;
-  eiler: Int64;
-  flag: boolean;
-  res: integer;
-  MArray: array of integer;
-  SignArray: array of int64;
-  maxInt64: Int64;
-  maxM: integer;   // 2^21 -1
-
-  isHashNegative : boolean;
-
-  //testers:
-  tried: cardinal;
-  trigger: boolean;
+  pathToOriginalFile : string;
 
 implementation
 
@@ -54,7 +41,7 @@ uses
 
 procedure reshErat(maxEl : integer);
 var
-  BB: array[1..50] of Integer;
+ // BB: array[1..50] of Integer;
   A: array of boolean;
   n, x, y: integer;
 begin
@@ -77,78 +64,10 @@ begin
     end;
     x := x-1;
   end;
-
 end;
 
-
-// решето Эратосфена как процедура поиска больших простых чисел
-procedure reshetoErat();
-var
-  A: array[1..232] of boolean;
-  n, x, y: integer;
-begin
-  A[1] := false;
-  n := 232;
-  for x := 2 to n do
-    A[x] := true;
-  for x := 2 to n div 2 do
-    for y := 2 to n div x do
-      A[x * y] := false;
-  y := 1;
-  for x := n downto n - 200 do
-    if A[x] then
-    begin
-      B[y] := x;
-      inc(y);
-    end;
-end;
-
-
-// наш 16 битный хеш
-function hashH37(s: string): integer;
-var
-  hash: integer;
-  i: cardinal;
-begin
-  hash := 139062143;
-  for i := 1 to length(s) do
-  begin
-    hash := 37 * hash + ord(s[i]);
-  end;
-  hash := hash shr 16;
-  hashH37 := hash;
-
-end;
-
-function crc16(s: string): integer;
-var
-  i, crc: integer;
-begin
-  crc := 0;
-  for i := 1 to length(s) do
-  begin
-    crc := crc + ord(s[i]) * 44111;
-    //crc:= crc xor (crc shr 8);
-
-  end;
-
-  Result := crc;
-end;
-
-function eightbitxorsum(mess: string): integer;
-var
-  i, sum, scode: integer;
-begin
-  sum := ord(mess[1]);
-  for i := 2 to length(mess) do
-    sum := sum xor ord(mess[i]);
-
-  Result := sum;
-end;
 
 function ToBin(x: integer): string;
-const
-  t: array[0..1] of char = ('0', '1');
 var
   res: string;
   d: 0..1;
@@ -157,14 +76,14 @@ begin
   while (x <> 0) do
   begin
     d := x mod 2;
-    res := t[d] + res;
+    res := IntToStr(d) + res;
     x := x div 2;
   end;
   Result := res;
 end;
 
 
-function modexp(x,y,n:int64; var z3 : int64):Int64;
+function modexp(x,y,n:int64):Int64;
 var z, k : int64;
 begin
   if (y = 0) then
@@ -172,41 +91,21 @@ begin
   else
   begin
     k:= y;
-    z := modexp(x, k div 2, n, z3);
+    z := modexp(x, k div 2, n);
     if ((y mod 2) = 0) then
       Result:= (z*z) mod n
     else
       Result:= (x*z*z) mod n;
   end;
-  z3:= Result;
 end;
 
-//   http://granitnayki.com/?p=212
-// ЭТА ХЕРНЯ ИЗОБРЕТЕНА УКРОПАМИ
-function niceLongPow(a : int64; n : Int64; m: int64):int64;
-var c, S, y : Int64;
-    i, k : integer;
-    strUp : string;
-begin
-  if (n = 1) then
-    Result:= a mod m
-  else
-  begin
-    strUp := ToBin(n);
-    k := length(strUp);
-    y:= a mod m;
-    for i:=k downto 2 do
-    begin
-      y:= (y*y) mod m;
-      if (strUp[i] = '1') then
-      begin
-        y:= y*a mod m;
-      end;
-    end;
-    Result:= y;
-  end;
-end;
-
+{
+  функция возвращает подстроку
+  from  - исходная строка
+  start - позиция от (вкл.)
+  endd  - позиция до (вкл.)
+  start и endd < длины from и > 0
+}
 function substring(from : string; start, endd : Cardinal):string;
 var i : Cardinal;
 begin
@@ -221,10 +120,10 @@ begin
 end;
 
 // хеширование строки методом SDBM (х32) с хорошим распределением по всем битам (мало коллизий)
-function SDBM(mess: string): int64;
+function SDBM(mess: string): integer;
 var
   i: cardinal;
-  hash: int64;
+  hash: integer;
 begin
   hash := 0;
   for i := 1 to Length(mess) do
@@ -232,7 +131,12 @@ begin
   Result := hash;
 end;
 
-// нод чисел алгоритмом 
+{
+  НОД чисел по алгоритму эвклида (расширенному ?)
+  x - множитель для a
+  y - множитель для b
+  gcd - НОД чисел
+}
 procedure gcd_ext(a, b: integer; var x, y, gcd: integer);
 var
   x1, y1: integer;
@@ -247,15 +151,6 @@ begin
   gcd_ext(b, a mod b, x1, y1, gcd);
   x := y1;
   y := x1 - (a div b) * y1
-end;
-
-// херня без задач
-function Znak(Val: Integer): Char;
-begin
-  if Val >= 0 then
-    Result := '+'
-  else
-    Result := '-';
 end;
 
 
@@ -275,40 +170,20 @@ begin
       Result := base * Result;
       if (result < 0) then
       begin
-        flag := true;
+        //flag := true;
         break;
       end;
     end;
   end;
 end;
 
-// возврат в число
-function toInt(ch: char): integer;
-begin
-  if ch = '0' then
-    Result := 0
-  else if ch = '1' then
-    Result := 1
-  else
-    Result := 9999;
-end;
-
-
-
-function genDigitalSignature(part : integer; dexp : integer; n : int64):Int64;
-var s : int64;
-begin
-  //s := niceLongPow(hashCode, dexp, n);
-  modexp(part, dexp, n, s);
-  Result:= s;
-end;
-
-
 
 procedure generateKeys(minimalN: integer; var n : Int64; var eexp : integer; var dexp : integer);
 var
   aa, bb, nod: integer;
   k1, k2 : Cardinal;
+  p, q : cardinal;
+  eiler : int64;
 begin
   Randomize();
   p := 0;
@@ -338,69 +213,123 @@ begin
 end;
 
 procedure createSign(minimalN: integer; var n : Int64; var eexp : integer; var dexp : integer);
-var
-  start, endd, full: integer;
-  hsh: int64;
 begin
   reshErat(Round(Sqrt(minimalN*10)));
   generateKeys(minimalN, n, eexp, dexp);
 end;
 
 
-function checkIt(part:integer; exp : Integer; targetMod : Int64) : int64;
-var
-  proizv: int64;
+{
+парсит пары вида [one;two], извлекая из них one и two
+отдельный случай: [;one;two] , указывает на то, что изначальный hashCode был отрицателен, записывается в neg
+}
+procedure parsePair(str : string; var one : Int64; var two : int64; var neg : boolean);
+var i : Cardinal;
+    Ch: Char;
+    s1, s2 : string;
+    index : Integer;
 begin
-  //proizv := niceLongPow(s, eexp, n);
-  modexp(part, exp, targetMod, proizv);
-  Result:= proizv;
+  i:= Length(str);
+
+  Ch := '0';
+  s1 := '';
+  s2 := '';
+  while (Ch <> '}') do
+  begin
+    Ch := str[i];
+    i := i - 1;
+  end;
+
+  Ch := str[i];
+  i := i - 1;
+  while (Ch <> ';') do
+  begin
+    s2 := Ch + s2;
+    Ch := str[i];
+    i := i - 1;
+  end;
+
+
+  Ch := str[i];
+  i := i - 1;
+  while (Ch <> '{') do
+  begin
+    // негативный флаг
+    if (Ch = ';') then
+    begin
+      neg := True;
+      Break;
+    end;
+    s1 := ch + s1;
+    Ch := str[i];
+    i := i - 1;
+  end;
+
+  // переводы значений из строк в int64
+  Val(s1, one, index);
+  if (index <> 0) then
+  begin
+    ShowMessage('One не число');
+  end;
+  Val(s2, two, index);
+  if (index <> 0) then
+  begin
+    ShowMessage('Two не число');
+  end;
+
 end;
 
+
+{
+создание электронно-цифровой подписи RSA для выбранного файла
+}
 procedure TForm1.Button1Click(Sender: TObject);
 var
-  b, c, d, z, index, llength: integer;
-  a: Int64;
-    // для разбиения на много m
-  currentM: integer;
-    // счетчик для разбиения
-  k, i: Cardinal;
-  el, elements: integer;
-  mesage: string;
-  proizv : int64;
-  f, f1 : TextFile;
+  f : TextFile;
   str : string;
   partOneStr : string;
   partTwoStr : string;
   strHashCode : string;
-  proizvStr : string;
-  sStr : string;
   s1, s2 : integer;
   nextIndex : Cardinal;
   partOne, partTwo : Integer;
-  maxInt32 : integer;
   // модуль хешкода, с ним мы и взаимодействуем
   HashNumber : Int64;
-
-
+  TStrList : TStringList;
+  mesage: string;
+  isHashNegative : Boolean;
+  hashCode : integer;
   n : Int64;
   dexp, eexp : Integer;
 begin
-  partOne:= 0;
-  partTwo:= 0;
-  if not (Form1.Edit3.Text = '') then
+  if (pathToOriginalFile = '') then
   begin
-    Val(Form1.Edit3.Text, hashCode, index);
-    if (index <> 0) then
-    begin
-      hashCode := SDBM(Form1.Edit3.Text);
-    end;
+    ShowMessage('откройте файл');
+    exit;
   end;
+  //partOne:= 0;
+  partTwo:= 0;
+  isHashNegative := False;
+
+  TStrList := TStringList.Create;
+  TStrList.LoadFromFile(pathToOriginalFile);
+  mesage := TStrList.Text;
+  TStrList.Free;
+
+  if (mesage = '') then
+  begin
+    ShowMessage('файл пуст');
+    exit;
+  end;
+  hashCode := SDBM(mesage);
 
 
   if (hashCode < 0) then
   begin
+    // отмечаем, что хеш был отрицательным
     isHashNegative:= True;
   end;
+  // наш алгоритм работает с положительным hash
   HashNumber:= Abs(hashCode);
   partOne:= HashNumber;
   partOneStr := IntToStr(HashNumber);
@@ -420,13 +349,15 @@ begin
   // формируем ключи по наибольшему из 2х значений
   createSign(Max(partOne, partTwo), n, eexp, dexp);
 
-  str := '{'+IntToStr(eexp)+';'+IntToStr(n)+'}';
+  // openedKey.txt
+  str := '{' +IntToStr(eexp)+';'+IntToStr(n) + '}';
   AssignFile(f, 'openedKey.txt');
   Rewrite(f);
   Append(f);
   Writeln(f, str);
   CloseFile(f);
 
+  // closedKey.txt
   str := '{'+IntToStr(dexp)+';'+IntToStr(n)+'}';
   AssignFile(f, 'closedKey.txt');
   Rewrite(f);
@@ -434,16 +365,83 @@ begin
   Writeln(f, str);
   CloseFile(f);
 
-
-  s1:= genDigitalSignature(partOne, dexp, n);
-  s2:= genDigitalSignature(partTwo, dexp, n);
-  str := '{'+IntToStr(s1)+';'+IntToStr(s2)+'}';
+  // RSA.txt
+  // формируем цифровую подпись
+  s1:= modexp(partOne, dexp, n);
+  s2:= modexp(partTwo, dexp, n);
+  str := '{';
+  if (isHashNegative) then
+    str := '{;';
+  str := str+IntToStr(s1)+';'+IntToStr(s2)+'}';
   AssignFile(f, 'RSA.txt');
   Rewrite(f);
   Append(f);
   Writeln(f, str);
   CloseFile(f);
 
+end;
+
+{
+проверка электронно-цифровой подписи RSA для выбранного файла
+}
+procedure TForm1.Button2Click(Sender: TObject);
+var hashPart1, hashPart2 : int64;
+    e, n : Int64;
+    proizv1, proizv2 : integer;
+    hashCode : Integer;
+    hashLen : Cardinal;
+    h1, h2, hsum : Integer;
+    ne, neg : Boolean;
+    TStrList : TStringList;
+    mesage : string;
+begin
+  if (pathToOriginalFile = '') then
+  begin
+    ShowMessage('откройте файл');
+    exit;
+  end;
+  TStrList := TStringList.Create;
+  TStrList.LoadFromFile(pathToOriginalFile);
+  mesage := TStrList.Text;
+  TStrList.Free;
+
+  if (mesage = '') then
+  begin
+    ShowMessage('файл пуст');
+    exit;
+  end;
+  hashCode := SDBM(mesage);
+  hashPart1 := 0;
+  hashPart2 := 0;
+  e := 0;
+  n := 0;
+  neg := False;
+  parsePair(Form1.Edit1.Text, hashPart1, hashPart2, neg);
+  parsePair(Form1.Edit2.Text, e, n, ne);
+  // осуществляем проверку
+  proizv1 := modexp(hashPart1, e, n);
+  proizv2 := modexp(hashPart2, e, n);
+
+  hashLen := Length(IntToStr(hashCode));
+  h1 := proizv1 * pow(10, hashLen - Length(IntToStr(proizv1)));
+  h2 := proizv2;
+  hsum := h1 + h2;
+  if (neg) then
+    hsum := -hsum;
+  if (hsum = hashCode) then
+    ShowMessage('документ подлинный');
+end;
+
+
+{
+Выбор файла из OpenDialog
+}
+procedure TForm1.Button3Click(Sender: TObject);
+begin
+ if OpenDialog1.Execute then
+ begin
+   pathToOriginalFile := OpenDialog1.FileName;
+ end;
 end;
 
 // http://www.cyberforum.ru/delphi-beginners/thread1116790.html
@@ -455,8 +453,172 @@ end;
 // http://teh-box.ru/informationsecurity/algoritm-shifrovaniya-rsa-na-palcax.html
 // https://planetcalc.ru/3311/
 
-
-
 // !!!! Молдовян Н. А. Теоретический минимум и алгоритмы цифровой подписи
 
+
+
+//vars:
+  // хеш строки
+  //p, q: integer;
+  //eiler: Int64;
+  //flag: boolean;
+  //res: integer;
+  //MArray: array of integer;
+  //SignArray: array of int64;
+  //maxInt64: Int64;
+  //maxM: integer;   // 2^21 -1
+  //isHashNegative : boolean;
+  //testers:
+  //tried: cardinal;
+  //trigger: boolean;
+
+  
+{
+// наш 16 битный хеш
+function hashH37(s: string): integer;
+var
+  hash: integer;
+  i: cardinal;
+begin
+  hash := 139062143;
+  for i := 1 to length(s) do
+  begin
+    hash := 37 * hash + ord(s[i]);
+  end;
+  hash := hash shr 16;
+  hashH37 := hash;
+end;
+}
+
+{
+// решето Эратосфена как процедура поиска больших простых чисел
+procedure reshetoErat();
+var
+  A: array[1..232] of boolean;
+  n, x, y: integer;
+begin
+  A[1] := false;
+  n := 232;
+  for x := 2 to n do
+    A[x] := true;
+  for x := 2 to n div 2 do
+    for y := 2 to n div x do
+      A[x * y] := false;
+  y := 1;
+  for x := n downto n - 200 do
+    if A[x] then
+    begin
+      B[y] := x;
+      inc(y);
+    end;
+end;
+}
+
+
+{
+function crc16(s: string): integer;
+var
+  i, crc: integer;
+begin
+  crc := 0;
+  for i := 1 to length(s) do
+  begin
+    crc := crc + ord(s[i]) * 44111;
+    //crc:= crc xor (crc shr 8);
+  end;
+
+  Result := crc;
+end;
+}
+
+{
+function eightbitxorsum(mess: string): integer;
+var
+  i, sum, scode: integer;
+begin
+  sum := ord(mess[1]);
+  for i := 2 to length(mess) do
+    sum := sum xor ord(mess[i]);
+
+  Result := sum;
+end;
+}
+
+
+{
+// возврат в число
+function toInt(ch: char): integer;
+begin
+  if ch = '0' then
+    Result := 0
+  else if ch = '1' then
+    Result := 1
+  else
+    Result := MaxLongint;
+end;
+}
+
+{
+// херня без задач
+function sign(Val: Integer): Char;
+begin
+  if Val >= 0 then
+    Result := '+'
+  else
+    Result := '-';
+end;
+}
+
+
+{
+// не рабочая по человечески функция, следует сравнить с modexp
+//   http://granitnayki.com/?p=212
+// ЭТА ХЕРНЯ ИЗОБРЕТЕНА УКРОПАМИ
+function niceLongPow(a : int64; n : Int64; m: int64):int64;
+var c, S, y : Int64;
+    i, k : integer;
+    strUp : string;
+begin
+  if (n = 1) then
+    Result:= a mod m
+  else
+  begin
+    strUp := ToBin(n);
+    k := length(strUp);
+    y:= a mod m;
+    for i:=k downto 2 do
+    begin
+      y:= (y*y) mod m;
+      if (strUp[i] = '1') then
+      begin
+        y:= y*a mod m;
+      end;
+    end;
+    Result:= y;
+  end;
+end;
+}
+
+{
+function genDigitalSignature(part : integer; dexp : integer; n : int64):Int64;
+begin
+  //s := niceLongPow(hashCode, dexp, n);
+  //modexp(part, dexp, n, s);
+  //Result:= s;
+  Result := modexp(part, dexp, n);
+end;
+}
+
+{
+function checkIt(part:integer; exp : Integer; targetMod : Int64) : int64;
+begin
+  //proizv := niceLongPow(s, eexp, n);
+  //modexp(part, exp, targetMod, proizv);
+  // Result:= proizv;
+  Result := modexp(part, exp, targetMod);
+end;
+}
+
+
+end.
 
--- src/Unit1.~dfm
+++ src/Unit1.~dfm
@@ -1,8 +1,8 @@
 object Form1: TForm1
-  Left = 1116
-  Top = 363
-  Width = 647
-  Height = 529
+  Left = 775
+  Top = 341
+  Width = 904
+  Height = 657
   Caption = 'Form1'
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
@@ -13,9 +13,23 @@ object Form1: TForm1
   OldCreateOrder = False
   PixelsPerInch = 120
   TextHeight = 16
+  object Label1: TLabel
+    Left = 400
+    Top = 280
+    Width = 187
+    Height = 16
+    Caption = #1069#1083#1077#1082#1090#1088#1086#1085#1085#1072#1103' '#1087#1086#1076#1087#1080#1089#1100' ('#1087#1072#1088#1072')'
+  end
+  object Label2: TLabel
+    Left = 400
+    Top = 344
+    Width = 132
+    Height = 16
+    Caption = #1054#1090#1082#1088#1099#1090#1099#1081' '#1082#1083#1102#1095' {e;n}'
+  end
   object Button1: TButton
-    Left = 176
-    Top = 64
+    Left = 72
+    Top = 288
     Width = 217
     Height = 25
     Caption = #1057#1086#1079#1076#1072#1090#1100' '#1101#1083'. '#1087#1086#1076#1087#1080#1089#1100
@@ -28,5 +42,41 @@ object Form1: TForm1
     Width = 121
     Height = 24
     TabOrder = 1
+  end
+  object Edit1: TEdit
+    Left = 440
+    Top = 304
+    Width = 121
+    Height = 24
+    TabOrder = 2
+  end
+  object Edit2: TEdit
+    Left = 440
+    Top = 368
+    Width = 121
+    Height = 24
+    TabOrder = 3
+  end
+  object Button2: TButton
+    Left = 464
+    Top = 408
+    Width = 81
+    Height = 25
+    Caption = #1055#1088#1086#1074#1077#1088#1080#1090#1100
+    TabOrder = 4
+    OnClick = Button2Click
+  end
+  object Button3: TButton
+    Left = 224
+    Top = 88
+    Width = 121
+    Height = 25
+    Caption = #1054#1090#1082#1088#1099#1090#1100' '#1092#1072#1081#1083
+    TabOrder = 5
+    OnClick = Button3Click
+  end
+  object OpenDialog1: TOpenDialog
+    Left = 48
+    Top = 136
   end
 end
